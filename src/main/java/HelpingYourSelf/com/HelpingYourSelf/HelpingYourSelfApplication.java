bde4d9bdb5077d63463850fdcfd773d1c5e92adc
khady2204 <khady.kandji3@unchk.edu.sn>
2025-06-11 12:43:45 +0000

    Initial commit - ajout du projet

---
+++ src/main/java/HelpingYourSelf/com/HelpingYourSelf/HelpingYourSelfApplication.java
@@ -0,0 +1,13 @@
+package HelpingYourSelf.com.HelpingYourSelf;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HelpingYourSelfApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HelpingYourSelfApplication.class, args);
+	}
+
+}
