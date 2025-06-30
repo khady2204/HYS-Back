ad01dc41e00566f47cadadd263549356ffb8ac51
Abdoulaye-Segnane <100135952+Abdoulaye-Segnane@users.noreply.github.com>
2025-06-30 03:46:19 +0000

    Revert "partie contact"

--- src/main/java/com/example/com/hysback/repository/ContactRepository.java
+++
@@ -1,9 +0,0 @@
-package com.example.hysback.repository;
-
-import com.example.hysback.entity.Contact;
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
-
-@Repository
-public interface ContactRepository extends JpaRepository<Contact, Long> {
-}
