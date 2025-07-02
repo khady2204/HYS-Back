91e0a1b90100f5baaa819c581563eb95584c0640
noreyni20 <modoukarakandji7@gmail.com>
2025-07-02 16:29:20 +0200

    Auth A Valider

--- src/main/java/HelpingYourSelf/com/HelpingYourSelf/HelpingYourSelfApplication.java
+++ src/main/java/HelpingYourSelf/com/HelpingYourSelf/HelpingYourSelfApplication.java
@@ -1,4 +1,4 @@
-package com.example.hysback;
+package HelpingYourSelf.com.HelpingYourSelf;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
--- src/main/java/com/example/com/hysback/repository/SuggestionRepository.java
+++
@@ -1,7 +0,0 @@
-package com.example.hysback.repository;
-
-import com.example.hysback.entity.Suggestion;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface SuggestionRepository extends JpaRepository<Suggestion, Long> {
-}
