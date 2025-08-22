e8058d4946701ce93647d84ca25e72958a3d07c7
projet-operationnel <projetoperationnel>
2025-08-22 16:01:33 +0000

    feacture: add story

---
+++ src/main/java/HelpingYourSelf/com/HelpingYourSelf/Security/SecurityUtils.java
@@ -0,0 +1,20 @@
+package HelpingYourSelf.com.HelpingYourSelf.Security;
+
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.stereotype.Component;
+
+@Component
+public class SecurityUtils {
+    public static Long getCurrentUserId() {
+        Authentication authentication = SecurityContextHolder.getContext().getAuthentication();
+        if (authentication != null && authentication.getPrincipal() instanceof UserDetails) {
+            UserDetails userDetails = (UserDetails) authentication.getPrincipal();
+            if (userDetails instanceof CustomUserDetails) {
+                return ((CustomUserDetails) userDetails).getUser().getId();
+            }
+        }
+        throw new IllegalStateException("Utilisateur non connecté");
+    }
+}
