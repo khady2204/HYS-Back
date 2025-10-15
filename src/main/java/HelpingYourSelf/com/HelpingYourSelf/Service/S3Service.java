761a73a3e958b5a87f8417cf2a2d6d6be1dd8e82
VETO18207 <71079446+VETO18207@users.noreply.github.com>
2025-10-15 11:19:24 +0000

    message image

--- src/main/java/HelpingYourSelf/com/HelpingYourSelf/Service/S3Service.java
+++ src/main/java/HelpingYourSelf/com/HelpingYourSelf/Service/S3Service.java
@@ -1,25 +1,37 @@
 package HelpingYourSelf.com.HelpingYourSelf.Service;
-import software.amazon.awssdk.core.sync.RequestBody;
-import software.amazon.awssdk.services.s3.S3Client;
-import software.amazon.awssdk.services.s3.model.PutObjectRequest;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Service;
 import org.springframework.web.multipart.MultipartFile;
+import software.amazon.awssdk.core.sync.RequestBody;
+import software.amazon.awssdk.services.s3.S3Client;
+import software.amazon.awssdk.services.s3.model.GetUrlRequest;
+import software.amazon.awssdk.services.s3.model.PutObjectRequest;
 
 import java.util.UUID;
 
 
 @Service
 public class S3Service {
-    @Value("${aws.s3.bucket-name:hsycompartiment}")
-    private String bucketName;
 
     private final S3Client s3Client;
 
-    public S3Service() {
-        this.s3Client = S3Client.builder()
-                .region(software.amazon.awssdk.regions.Region.AF_SOUTH_1)
-                .build();
+    @Value("${aws.s3.bucket-name:hsycompartiment}")
+    private String bucketName;
+
+    @Value("${aws.s3.profiles-folder:profiles}")
+    private String profilesFolder;
+
+    @Value("${aws.s3.status-folder:status_media}")
+    private String statusFolder;
+
+    @Value("${aws.s3.publications-folder:publications}")
+    private String publicationsFolder;
+
+    @Value("${aws.s3.messages-folder:messages}")
+    private String messagesFolder;
+
+    public S3Service(S3Client s3Client) {
+        this.s3Client = s3Client;
     }
 
     public String uploadFile(MultipartFile file, String folder) {
@@ -41,22 +53,27 @@ public class S3Service {
     }
 
     private String generatePublicUrl(String fileName) {
-        return String.format("https://%s.s3.af-south-1.amazonaws.com/%s", bucketName, fileName);
+        return s3Client.utilities()
+                .getUrl(GetUrlRequest.builder()
+                        .bucket(bucketName)
+                        .key(fileName)
+                        .build())
+                .toExternalForm();
     }
 
     public String uploadProfileImage(MultipartFile file) {
-        return uploadFile(file, "profiles");
+        return uploadFile(file, profilesFolder);
     }
 
     public String uploadStatusMedia(MultipartFile file) {
-        return uploadFile(file, "status_media");
+        return uploadFile(file, statusFolder);
     }
 
     public String uploadPublicationMedia(MultipartFile file) {
-        return uploadFile(file, "publications");
+        return uploadFile(file, publicationsFolder);
     }
 
     public String uploadMessageMedia(MultipartFile file) {
-        return uploadFile(file, "messages");
+        return uploadFile(file, messagesFolder);
     }
 }
